d81339027de84d2afba171209cae4efea2ec129e
devil <europedreadlydevil@gmail.com>
2025-06-15 16:49:23 +0300

    Refactor: Remove unnecessary dependency and import
    The `lazy_static` dependency was removed from `Cargo.toml` as it is no
    longer needed. The `Write` trait from `std::fmt` was also removed from
    the test file as it was not being used.

--- src/writer/test/mod.rs
+++ src/writer/test/mod.rs
@@ -1,4 +1,3 @@
-use std::fmt::Write;
 use crate::buffer::{CharBuffer, BUFFER_HEIGHT};
 use crate::buffer::mock::MockBuffer;
 use crate::color::ColorCode;
@@ -8,15 +7,15 @@ use crate::writer::VGAWriter;
 #[test]
 fn write_char() {
     static mut MOCK_BUFFER: MockBuffer = MockBuffer::default();
-    
+
     #[allow(unused_unsafe)]
     let mock_buffer= unsafe { &raw mut MOCK_BUFFER };
-    
+
     let mut writer = VGAWriter::new(mock_buffer);
     writer.write_char('H');
-    
+
     assert_eq!(
-        unsafe { (*mock_buffer).get_char_at(0,0) }, 
+        unsafe { (*mock_buffer).get_char_at(0,0) },
         ScreenChar::new(b'H', ColorCode::default())
     )
 }
@@ -57,11 +56,11 @@ fn chars_overflow() {
     let mock_buffer= unsafe { &raw mut MOCK_BUFFER };
 
     let mut writer = VGAWriter::new(mock_buffer);
-    
+
     for _ in 0..BUFFER_HEIGHT {
         writer.write_string("Hey\n");
     }
-    
+
     for row in 0..BUFFER_HEIGHT-1 {
         assert_eq!(
             unsafe { (*mock_buffer).get_char_at(row,0) },
@@ -80,7 +79,7 @@ fn chars_overflow() {
     }
     
     writer.write_char('!');
-
+    
     assert_eq!(
         unsafe { (*mock_buffer).get_char_at(BUFFER_HEIGHT-1,0) },
         ScreenChar::new(b'!', ColorCode::default())
