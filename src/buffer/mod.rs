eb3cc74eeb70b5bed1e4baa04f2576989907d52b
devil <europedreadlydevil@gmail.com>
2025-06-15 16:58:51 +0300

    Bump version to 0.1.11
    Make CharBuffer trait Send
    
    The CharBuffer trait needs to be Send to be used in a concurrent context.

--- src/buffer/mod.rs
+++ src/buffer/mod.rs
@@ -7,7 +7,7 @@ pub(crate) mod mock;
 pub(crate) const BUFFER_HEIGHT: usize = 25;
 pub(crate) const BUFFER_WIDTH: usize = 80;
 
-pub(crate) trait CharBuffer {
+pub(crate) trait CharBuffer: Send {
     fn set_char_at(&mut self, row: usize, col: usize, char: ScreenChar);
     fn get_char_at(&self, row: usize, col: usize) -> ScreenChar;
 }
